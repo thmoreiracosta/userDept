ecd73dbd9d55a0c7c614b4701fc83d8b6bc05793
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-06-01 17:00:14 -0300

    End's points - criação

--- backend/src/main/java/com/devsuperior/userdept/entities/Department.java
+++ backend/src/main/java/com/devsuperior/userdept/entities/Department.java
@@ -1,7 +1,17 @@
 package com.devsuperior.userdept.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_department")
 public class Department {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
--- backend/src/main/java/com/devsuperior/userdept/entities/User.java
+++ backend/src/main/java/com/devsuperior/userdept/entities/User.java
@@ -1,11 +1,25 @@
 package com.devsuperior.userdept.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+
 public class User {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	private String name;
 	private String email;
 	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department department;
 	
 	public User() {
